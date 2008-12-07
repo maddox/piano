3de277f25be431e7601d681fbea428e342ca59db
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-12-07 02:13:55 -0500

    Revert "added login credentials"
    This reverts commit 3c96f8454076bf71c1604c2d88772ba4c94cf02b.

--- piano.rb
+++ piano.rb
@@ -6,12 +6,6 @@ require 'lib/models'
 # layout 'layout'
 
 get '/' do
-  use Rack::Auth::Basic do |username, password|
-    username == 'admin' && password == 'secret'
-  end
-
   @products = Product.find(:all)
   erb :index
 end
-
-
