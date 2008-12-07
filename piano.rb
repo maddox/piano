3c96f8454076bf71c1604c2d88772ba4c94cf02b
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-12-07 02:13:24 -0500

    added login credentials

--- piano.rb
+++ piano.rb
@@ -6,6 +6,12 @@ require 'lib/models'
 # layout 'layout'
 
 get '/' do
+  use Rack::Auth::Basic do |username, password|
+    username == 'admin' && password == 'secret'
+  end
+
   @products = Product.find(:all)
   erb :index
 end
+
+
